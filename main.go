f7818795222f7931c8f71a92b556bf4a3f66954e
Nate Geslin <ngeslin@nerdery.com>
2018-03-10 22:28:52 -0600

    begins adding types and more succinct functions

--- main.go
+++ main.go
@@ -7,20 +7,57 @@ import (
 	"strings"
 )
 
+// GitBranch is a representation of a single git branch
+type GitBranch struct {
+	Rawname string
+}
+
+// GitBranchCollection is a Collection object of `GitBranch` instances
+type GitBranchCollection struct {
+	items []GitBranch
+}
+
+// AddItem `GitBranch` instance to collection
+func (gbc *GitBranchCollection) AddItem(gitBranch GitBranch) {
+	gbc.items = append(gbc.items, gitBranch)
+}
+
+var collection GitBranchCollection
+
+func init() {
+	run()
+}
+
 func main() {
+	fmt.Printf("%v \n\n\n", collection.items)
+	fmt.Print("Now were ready...")
+}
+
+func run() {
 	out, err := exec.Command("git", "branch").Output()
 
 	if err != nil {
 		log.Fatalf("cmd.Output() failed with %s\n", err)
 	}
 
-	str := string(out)
+	branchList := readLocalGitBranchList(out)
+
+	hydrateBranchCollection(branchList)
+}
+
+func readLocalGitBranchList(cmdOut []byte) []string {
+	str := string(cmdOut)
 	trimmedStr := strings.TrimRight(str, "\n")
-	result := strings.Split(trimmedStr, "\n")
+	localGitBranchList := strings.Split(trimmedStr, "\n")
 
-	for i := range result {
-		fmt.Printf("\n%v - %v\n", i, result[i])
+	return localGitBranchList
+}
+
+func hydrateBranchCollection(branchList []string) {
+	for i := range branchList {
+		s := strings.TrimLeft(branchList[i], " ")
+		gb := GitBranch{s}
+
+		collection.AddItem(gb)
 	}
-
-	fmt.Println(len(result))
 }
